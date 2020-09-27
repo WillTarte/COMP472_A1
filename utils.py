dab6e48715906cf1ebc55026838386644cc32327
WillTarte <William.tarte6@gmail.com>
2020-09-27 19:07:45 -0400

    MLP works, still need to test with testing data

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def getInfo(fileName):
     datasetDirectory = './dataset/'
-    datasetFullName = datasetDirectory + fileName;
+    datasetFullName = datasetDirectory + fileName
     data = np.genfromtxt(datasetFullName, delimiter=',', dtype=None, encoding=None)
     #Now we are going to store the values in a keypair value
     #Note that we start at index 1 in order to avoid storing the headers into our dictionary
